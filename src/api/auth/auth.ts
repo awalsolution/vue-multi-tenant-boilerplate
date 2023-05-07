af7b8eb4680279e00ef691270c6592e81287eb41
Iqbal <iqbal.hassan2584@gmail.com>
2023-05-07 02:01:07 -0700

    add auth endpoints

---
+++ src/api/auth/auth.ts
@@ -0,0 +1,81 @@
+import { http } from '@/utils/http/axios';
+
+export interface BasicResponseModel<T = any> {
+  code: number;
+  message: string;
+  result: T;
+}
+
+export interface BasicPageParams {
+  pageNumber: number;
+  pageSize: number;
+  total: number;
+}
+
+/**
+ * @description: Get user information
+ */
+export function getUserInfo() {
+  return http.request({
+    url: '/users/authenticated',
+    method: 'get',
+  });
+}
+
+/**
+ * @description: User login
+ */
+export function login(params: any) {
+  return http.request<BasicResponseModel>(
+    {
+      url: '/auth/login',
+      method: 'POST',
+      params,
+    },
+    {
+      isTransformResponse: false,
+    }
+  );
+}
+/**
+ * @description: User register
+ */
+export function register(params: any) {
+  return http.request<BasicResponseModel>(
+    {
+      url: '/auth/register',
+      method: 'POST',
+      params,
+    },
+    {
+      isTransformResponse: false,
+    }
+  );
+}
+
+/**
+ * @description: user logout
+ */
+export function logout(params: any) {
+  return http.request({
+    url: '/auth/logout',
+    method: 'POST',
+    params,
+  });
+}
+
+/**
+ * @description: user change password
+ */
+export function changePassword(params: any, uid: any) {
+  return http.request(
+    {
+      url: `/user/u${uid}/changepw`,
+      method: 'POST',
+      params,
+    },
+    {
+      isTransformResponse: false,
+    }
+  );
+}
