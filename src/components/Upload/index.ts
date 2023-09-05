2ce49b92767cab9b3c22903c9fe006b5cab2b2b0
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-09-05 19:03:14 +0500

    remove v1 code

--- src/components/Upload/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SingleImageUploader } from '@/components/upload/SingleImageUploader/SingleImageUploader.vue';
-export { default as MultiImageUploader } from '@/components/upload/MultiImageUploader/MultiImageUploader.vue';
--- src/config/website.config.ts
+++
@@ -1,9 +0,0 @@
-import logoImage from '@/assets/images/logo.png';
-import loginImage from '@/assets/images/account-logo.png';
-
-export const websiteConfig = Object.freeze({
-  title: 'InSync Admin',
-  logo: logoImage,
-  loginImage: loginImage,
-  loginDesc: 'InSync Admin CRM solution',
-});
