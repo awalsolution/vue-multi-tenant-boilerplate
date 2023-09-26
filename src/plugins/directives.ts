1eea9573d1f1ab7e51d8b089e4ed3815a0338a96
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-09-26 22:58:58 +0500

    remove v2 code

--- src/plugins/directives.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue';
-
-import { permission } from '@src/directives/permission';
-
-export async function setupDirectives(app: App) {
-  // permission control command (demo)
-  app.directive('permission', permission);
-}
