0eccb05ad6484ed0c2ff0217cb2b9453ec34b23c
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-12-21 14:49:23 +0500

    fix

--- src/config/website.config.ts
+++ src/config/website.config.ts
@@ -2,8 +2,8 @@ import logoImage from '@src/assets/images/bit_ocean.png';
 import loginImage from '@src/assets/images/bit_ocean.png';
 
 export const websiteConfig = Object.freeze({
-  title: 'InSync Admin',
+  title: 'AbendSoft Private Limited Admin CRM',
   logo: logoImage,
   loginImage: loginImage,
-  loginDesc: 'InSync Admin CRM Solution'
+  loginDesc: 'AbendSoft Private Limited CRM Solution'
 });
