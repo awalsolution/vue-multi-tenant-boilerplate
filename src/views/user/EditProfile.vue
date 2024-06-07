db1502f98fdb5c5f1bf1c2f3734f06575cf39f3a
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2024-06-07 16:51:33 +0500

    fix profile issue

--- src/views/user/EditProfile.vue
+++ src/views/user/EditProfile.vue
@@ -1,63 +1,61 @@
 <template>
-  <n-space :vertical="true">
-    <n-form ref="formRef" :label-width="80" :model="profileData" :rules="formRules" size="small">
-      <n-card title="Edit Profile">
-        <n-row gutter="12">
-          <n-col :span="8">
-            <n-form-item label="First Name" path="first_name">
-              <n-input v-model:value="profileData.first_name" placeholder="Enter First Name" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="8">
-            <n-form-item label="Last Name" path="last_name">
-              <n-input v-model:value="profileData.last_name" placeholder="Enter Last Name" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="8">
-            <n-form-item label="Phone Number" path="phone_number">
-              <n-input v-model:value="profileData.phone_number" placeholder="Enter Phone Number" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="8">
-            <n-form-item label="Address" path="address">
-              <n-input v-model:value="profileData.address" placeholder="Enter Address" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="8">
-            <n-form-item label="City" path="city">
-              <n-input v-model:value="profileData.city" placeholder="Enter City" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="8">
-            <n-form-item label="State" path="state">
-              <n-input v-model:value="profileData.state" placeholder="Enter State" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="24">
-            <n-form-item label="Country" path="country">
-              <n-input v-model:value="profileData.country" placeholder="Enter Country" />
-            </n-form-item>
-          </n-col>
-          <n-col :span="24">
-            <SingleImageUploader
-              :action="uploadUrl"
-              :data="{ type: 0 }"
-              name="profile_image"
-              :width="100"
-              :height="100"
-              @upload-change="uploadChange"
-              v-model:value="profileData.profile_picture"
-            />
-          </n-col>
-        </n-row>
-      </n-card>
-      <n-space justify="end">
-        <n-form-item :theme-overrides="{ labelHeightSmall: '0', feedbackHeightSmall: '0' }">
-          <n-button secondary type="info" @click="handleValidateClick"> Save</n-button>
-        </n-form-item>
-      </n-space>
-    </n-form>
-  </n-space>
+  <n-form ref="formRef" :label-width="80" :model="profileData" :rules="formRules" size="small">
+    <n-card title="Edit Profile">
+      <n-row gutter="12">
+        <n-col :span="8">
+          <n-form-item label="First Name" path="first_name">
+            <n-input v-model:value="profileData.first_name" placeholder="Enter First Name" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="8">
+          <n-form-item label="Last Name" path="last_name">
+            <n-input v-model:value="profileData.last_name" placeholder="Enter Last Name" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="8">
+          <n-form-item label="Phone Number" path="phone_number">
+            <n-input v-model:value="profileData.phone_number" placeholder="Enter Phone Number" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="8">
+          <n-form-item label="Address" path="address">
+            <n-input v-model:value="profileData.address" placeholder="Enter Address" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="8">
+          <n-form-item label="City" path="city">
+            <n-input v-model:value="profileData.city" placeholder="Enter City" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="8">
+          <n-form-item label="State" path="state">
+            <n-input v-model:value="profileData.state" placeholder="Enter State" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="24">
+          <n-form-item label="Country" path="country">
+            <n-input v-model:value="profileData.country" placeholder="Enter Country" />
+          </n-form-item>
+        </n-col>
+        <n-col :span="24">
+          <SingleImageUploader
+            :action="uploadUrl"
+            :data="{ type: 0 }"
+            name="profile_image"
+            :width="100"
+            :height="100"
+            @upload-change="uploadChange"
+            v-model:value="profileData.profile_picture"
+          />
+        </n-col>
+      </n-row>
+    </n-card>
+    <n-space justify="end" class="mt-4">
+      <n-form-item :theme-overrides="{ labelHeightSmall: '0', feedbackHeightSmall: '0' }">
+        <n-button secondary type="info" @click="handleValidateClick"> Save</n-button>
+      </n-form-item>
+    </n-space>
+  </n-form>
 </template>
 
 <script lang="ts" setup>
